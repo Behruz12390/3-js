2031c2473807f66224d3f73150c83d70a7b30924
Behruz12390 <168063527+Behruz12390@users.noreply.github.com>
2024-06-27 12:55:12 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+let a = Math.round(Math.random() * 100)
+alert('Sizda xozi random sonlar chiqishni boshledi')
+alert(`Random orqali chiqqan son: ${a}`)
+confirm('Biz random sonni 2 ga kopaytirmoqchimiz va bolmoqchimiz!')
+alert(`Random orqali chiqqan son: ${a} edi` + `\nBiz uni 2 ga kopaytirib ${a * 2} sonini chiqardik` + `\nVa shu sonni 2 ga bolib: ${a / 2} chiqardik` + `\nVa shu sonni 3 ga bolganda qoldiq: ${a % 3} sonini chiqardik`)
